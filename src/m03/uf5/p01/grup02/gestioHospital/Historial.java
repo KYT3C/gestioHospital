f053e1e01e57e53a8ec58d56289ea3fbe266d3ef
K Y T 3 C <gonzalo.garcia@copernic.cat>
2018-12-03 11:42:22 +0100

    Creada las Class : Adreca , Historial , Hospital , Malaltia , Metge , Pacient , Persona , Visita.

---
+++ src/m03/uf5/p01/grup02/gestioHospital/Historial.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m03.uf5.p01.grup02.gestioHospital;
+
+/**
+ *
+ * @author K Y T 3 C
+ */
+public class Historial {
+    
+}
