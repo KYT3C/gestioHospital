2be585c48b2504ddd7410fe7c47f26522019b502
Antonio <toni.cano.redondo@gmail.com>
2018-12-10 09:11:19 +0100

    Malatia i hospital mas completos

--- src/m03/uf5/p01/grup02/gestioHospital/Hospital.java
+++ src/m03/uf5/p01/grup02/gestioHospital/Hospital.java
@@ -1,9 +1,40 @@
 package m03.uf5.p01.grup02.gestioHospital;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author sergi.f.sellares
  */
 public class Hospital {
+    String nom;
+    ArrayList<Pacient> pacients;
+    ArrayList<Historial> historials;
+    ArrayList<Metge> metges;
+    ArrayList<Malaltia> malalties;
+    Adreca adreca;
     
+    public Hospital(String nom){
+        this.nom = nom;
+        pacients = new ArrayList<>();
+        historials = new ArrayList<>();
+        metges = new ArrayList<>();
+        malalties = new ArrayList<>();
+    }
+    
+    public void afegirPacient(Pacient pacient){
+        pacients.add(pacient);
+    }
+    
+    public void afegirMetge(Metge metge){
+        metges.add(metge);
+    }
+    
+    public void afegirMalaltia(Malaltia malaltia){
+        malalties.add(malaltia);
+    }
+    
+    public void afegirHistorial(Historial historial){
+        historials.add(historial);
+    }
 }
--- src/m03/uf5/p01/grup02/gestioHospital/Malaltia.java
+++ src/m03/uf5/p01/grup02/gestioHospital/Malaltia.java
@@ -5,5 +5,34 @@ package m03.uf5.p01.grup02.gestioHospital;
  * @author sergi.f.sellares
  */
 public class Malaltia {
+    static int numMalaltia = 0;
+    int codi;
+    String nom;
+    boolean donaBaixa;
+    String tractament;
+    int duradaDies;
     
+    public Malaltia(String nom, boolean donaBaixa, String tractament, int duradaDies){
+        codi = numMalaltia++;
+        this.nom = nom;
+        this.donaBaixa = donaBaixa;
+        this.tractament = tractament;
+        this.duradaDies = duradaDies;
+    }
+    
+    public int getCodi(){
+        return codi;
+    }
+    
+    public boolean motiuDeBaixaLaboral(){
+        return donaBaixa;
+    }
+    
+    public String tractamentASeguir(){
+        return tractament;
+    }
+    
+    public int duradaDeTractament(){
+        return duradaDies;
+    }
 }
