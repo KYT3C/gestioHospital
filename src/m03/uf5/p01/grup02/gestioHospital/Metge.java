64be9174c2614cf772acc1caea532af2c16d3787
sergi.f.sellares <sergi.f.sellares@DESKTOP-DRJR1V9>
2018-12-14 13:46:38 +0100

    Afegit el mètode toString() i els getters

--- src/m03/uf5/p01/grup02/gestioHospital/Metge.java
+++ src/m03/uf5/p01/grup02/gestioHospital/Metge.java
@@ -1,13 +1,11 @@
 package m03.uf5.p01.grup02.gestioHospital;
 
-import java.util.Scanner;
-
 /**
  *
  * @author K Y T 3 C
  */
-public class Metge extends Persona{
-    
+public class Metge extends Persona {
+
     private int numEmpleat;
     private int salariMensual;
     private String codiCompteCorrent;
@@ -15,12 +13,43 @@ public class Metge extends Persona{
     public Metge(String nom, String cognom1, String cognom2, String numSegSocial, String nif, String telefon) {
         super(nom, cognom1, cognom2, numSegSocial, nif, telefon);
     }
-    
-    public int getNumEmpleat(){
+
+    public int getNumEmpleat() {
         return numEmpleat;
+    }
+
+    public int getSalariMensual() {
+        return salariMensual;
+    }
+
+    public String getCodiCompteCorrent() {
+        return codiCompteCorrent;
     }
     
     @Override
+    public String toString() {
+        String chain;
+        chain = getNom();
+        chain = chain + ",";
+        chain = chain + getCognom1();
+        chain = chain + ",";
+        chain = chain + getCognom2();
+        chain = chain + ",";
+        chain = chain + getNumSegSocial();
+        chain = chain + ",";
+        chain = chain + getNif();
+        chain = chain + ",";
+        chain = chain + getTelefon();
+        chain = chain + ",";
+        chain = chain + getNumEmpleat();
+        chain = chain + ",";
+        chain = chain + getSalariMensual();
+        chain = chain + ",";
+        chain = chain + getCodiCompteCorrent();
+        return chain;
+    }
+
+    /*@Override
    public String toString(){
        Scanner keyboard = new Scanner(System.in);
         int option;
@@ -50,6 +79,5 @@ public class Metge extends Persona{
             }
         }while(option != 4);
         return null;
-   
-   }
+   }*/
 }
